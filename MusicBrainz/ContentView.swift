022f7f371cbb31bf35c90cebdc86d47f6a0dcc34
akshaaatt <tiwariakshat03@gmail.com>
2022-01-18 18:56:43 +0530

    Initial Commit

---
+++ MusicBrainz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicBrainz
+//
+//  Created by Akshat Tiwari on 18/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicBrainz/MusicBrainzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBrainzApp.swift
+//  MusicBrainz
+//
+//  Created by Akshat Tiwari on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicBrainzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
